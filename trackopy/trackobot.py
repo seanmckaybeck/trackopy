8bdf9b655995484fe700900c5a84328f41de06e4
Sean Beck <seanmckaybeck@gmail.com>
2017-01-27 21:20:49 +0000

    added logging
    fixes gh-8

--- trackopy/trackobot.py
+++ trackopy/trackobot.py
@@ -1,14 +1,22 @@
 import datetime
+import logging
 
 import requests
 
 
+logger = logging.getLogger(__name__)
+
+
 class Trackobot:
     def __init__(self, username, password):
+        logger.info('Creating Trackobot instance')
         self._url = 'https://trackobot.com'
-        r = requests.post(self._url+'/sessions', data={'username': username, 'password': password})
+        endpoint = '/sessions'
+        logger.debug('POST on %s', endpoint)
+        r = requests.post(self._url+endpoint, data={'username': username, 'password': password})
         r.raise_for_status()
         if 'Invalid credentials' in r.text:
+            logger.error('Invalid credentials supplied')
             raise ValueError('Incorrect username or password. API token is not supported.')
         self._username = username
         self._password = password
@@ -23,7 +31,10 @@ class Trackobot:
         :return: Dictionary of new user data
         :raises: requests.exceptions.HTTPError on error
         """
-        url = 'https://trackobot.com/users.json'
+        logger.info('Called create_user()')
+        endpoint = '/users.json'
+        url = 'https://trackobot.com' + endpoint
+        logger.debug('POST on %s', endpoint)
         r = requests.post(url)
         r.raise_for_status()
         return r.json()
@@ -35,11 +46,15 @@ class Trackobot:
         :param str name: The new display name
         :return: None
         """
+        logger.info('Called rename_user()')
+        logger.debug('GET on /profile to get user\'s ID')
         r = requests.get('https://trackobot.com/profile', auth=self._auth)
         user_id = r.text.split('edit_user_',1)[1].split('"')[0]
+        logger.debug('User ID is %s', user_id)
         endpoint = '/users/{}/rename'.format(user_id)
         url = self._url + endpoint
         data = {'_method': 'patch', 'user[displayname]': name}
+        logger.debug('POST on %s', endpoint)
         r = requests.post(url, auth=self._auth, data=data)
         r.raise_for_status()
 
@@ -51,8 +66,10 @@ class Trackobot:
         :rtype: str
         :raises: requests.exceptions.HTTPError on error
         """
+        logger.info('Called one_time_auth()')
         endpoint = '/one_time_auth.json'
         url = self._url + endpoint
+        logger.debug('POST on %s', endpoint)
         r = requests.post(url, auth=self._auth)
         r.raise_for_status()
         return r.json()['url'] if 'error' not in r.json() else r.json()['error']
@@ -73,18 +90,23 @@ class Trackobot:
         :rtype: bool
         :raises: ValueError
         """
+        logger.info('Called modify_metadata()')
         allowed_params = ['added', 'mode', 'win', 'hero', 'opponenet',
                           'coin', 'duration', 'rank', 'legend', 'deck_id',
                           'opponent_deck_id', 'note']
         if param not in allowed_params:
+            logger.error('Parameter %s is not an approved value', param)
             raise ValueError('param must be one of ' + ', '.join(allowed_params))
         endpoint = '/profile/results/'
         url = self._url + endpoint + str(game_id)
         data = {param: value}
+        logger.debug('PUT on %s', endpoint)
         r = requests.put(url, auth=self._auth, json=data)
         if r.status_code == 204:
+            logger.info('Modify succeeded')
             return True
         else:
+            logger.info('Modify failed')
             return False
 
     def stats(self, stats_type: str='decks', time_range: str='all', mode: str='all',
@@ -111,17 +133,22 @@ class Trackobot:
         :raises: ValueError
         :raises: TypeError
         """
+        logger.info('Called stats()')
         allowed_endpoints = ['classes', 'decks', 'arena']
         allowed_range = ['current_month', 'all', 'last_3_days', 'last_24_hours', 'custom']
         allowed_modes = ['ranked', 'arena', 'casual', 'friendly', 'all']
         if stats_type not in allowed_endpoints:
+            logger.error('%s is not an allowed stats_type', stats_type)
             raise ValueError('stats_type must be one of ' + ', '.join(allowed_endpoints))
         if time_range not in allowed_range:
+            logger.error('%s is not an allowed time_range', time_range)
             raise ValueError('time_range must be one of ' + ', '.join(allowed_range))
         if mode not in allowed_modes:
+            logger.error('%s is not an allowed mode', mode)
             raise ValueError('mode must be one of ' + ', '.join(allowed_modes))
         if time_range == 'custom' and \
             (start is None or end is None or type(start) != datetime.datetime or type(end) != datetime.datetime):
+            logger.error('start and end must be of type datetime.datetime when using custom time_range')
             raise TypeError('If using "custom" mode then you must specify a datetime.datetime for start and end')
         endpoint = '/profile/stats/{}.json'.format(stats_type)
         params = {'mode': mode, 'time_range': time_range}
@@ -132,6 +159,7 @@ class Trackobot:
         elif stats_type == 'classes':
             params.update({'as_hero': as_hero, 'vs_hero': vs_hero})
         url = self._url + endpoint
+        logger.debug('GET on %s', endpoint)
         r = requests.get(url, auth=self._auth, params=params)
         r.raise_for_status()
         return r.json()
@@ -144,8 +172,10 @@ class Trackobot:
         :rtype: dict
         :raises: requests.exceptions.HTTPError on error
         """
+        logger.info('Called decks()')
         endpoint = '/profile/settings/decks.json'
         url = self._url + endpoint
+        logger.debug('GET on %s', endpoint)
         r = requests.get(url, auth=self._auth)
         r.raise_for_status()
         return r.json()
@@ -161,14 +191,17 @@ class Trackobot:
         :raises: requests.exceptions.HTTPError on error
         :raises: ValueError
         """
+        logger.info('Called reset()')
         allowed = ['ranked', 'casual', 'practice', 'arena', 'friendly']
         if modes is None:
             modes = allowed
         if any(mode not in allowed for mode in modes):
+            logger.error('Invalid mode(s) given')
             raise ValueError('modes list can only contain ' + ', '.join(allowed))
         endpoint = '/profile/settings/account/reset'
         url = self._url + endpoint
         data = {'reset_modes[]': modes}
+        logger.debug('POST on %s', endpoint)
         r = requests.post(url, auth=self._auth, data=data)
         r.raise_for_status()
 
@@ -185,11 +218,13 @@ class Trackobot:
         :rtype: dict
         :raises: requests.exceptions.HTTPError on error
         """
+        logger.info('Called history()')
         endpoint = '/profile.json' 
         url = self._url + endpoint
         params = {'page': page}
         if query is not None:
             params['query'] = query
+        logger.debug('GET on %s', endpoint)
         r = requests.get(url, auth=self._auth, params=params)
         r.raise_for_status()
         return r.json()
@@ -204,9 +239,11 @@ class Trackobot:
         :rtype: dict
         :raises: requests.exceptions.HTTPError on error
         """
+        logger.info('Called arena_history()')
         endpoint = '/profile/arena.json' 
         url = self._url + endpoint
         params = {'page': page}
+        logger.debug('GET on %s', endpoint)
         r = requests.get(url, auth=self._auth, params=params)
         r.raise_for_status()
         return r.json()
@@ -219,10 +256,12 @@ class Trackobot:
         :return None:
         :raises: requests.exceptions.HTTPError on error
         """
+        logger.info('Called toggle_tracking()')
         endpoint = '/profile/settings/decks/toggle'
         url = self._url + endpoint
         val = 'true' if enabled else 'false'
         data = {'user[deck_tracking]': val, '_method': 'put'}
+        logger.debug('POST on %s', endpoint)
         r = requests.post(url, auth=self._auth, data=data)
         r.raise_for_status()
 
@@ -233,8 +272,10 @@ class Trackobot:
         :param int game_id: The ID of the game to delete
         :return: None
         """
+        logger.info('Called delete_game()')
         endpoint = '/profile/results/' + str(game_id)
         url = self._url + endpoint
+        logger.debug('DELETE on %s', endpoint)
         r = requests.delete(url, auth=self._auth)
         r.raise_for_status()
 
@@ -251,8 +292,10 @@ class Trackobot:
         :rtype: dict
         :raises: requests.exceptions.HTTPError on error
         """
+        logger.info('Called upload_game()')
         endpoint = '/profile/results.json'
         url = self._url + endpoint
+        logger.debug('POST on %s', endpoint)
         r = requests.post(url, auth=self._auth, json=game_data)
         r.raise_for_status()
         return r.json()
